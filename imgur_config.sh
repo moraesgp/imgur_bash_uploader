ababfd042ba247c83cab67e1ed23000b6355bb18
Gabriel Moraes <moraesgp@gmail.com>
2012-06-25 17:07:38 -0300

    updated imgur_config.sh

--- imgur_config.sh
+++ imgur_config.sh
@@ -1,14 +1,27 @@
 
+# Get application keys from imgur.com and update below
 export OAUTH_CONSUMER_KEY="GET-YOUR-OWN-OAUTH_CONSUMER_KEY"
 export OAUTH_CONSUMER_SECRET="GET-YOUR-OWN-OAUTH_CONSUMER_SECRET"
 
+# Update below path with the directory where pictures are
+export PHOTO_ROOT_DIR=/home/moraesgp/pictures-test
+export MOVE_PHOTO_DIR=/home/moraesgp/pictures-test-moved
+
+# Change the logging diretory if you want but it's not mandatory
+export LOGTEMPDIR=logtmp
+
+# You should leave the rest of this file alone
 export OAUTH_SIGN_METHOD="HMAC-SHA1"
 export OAUTH_VERSION="1.0"
 
-export LOGTEMPDIR=logtmp
 export REQUEST_TOKEN_URL="https://api.imgur.com/oauth/request_token"
 export AUTHORIZE_URL="https://api.imgur.com/oauth/authorize"
 export ACCESS_TOKEN_URL="https://api.imgur.com/oauth/access_token"
+export IMAGES_API_URL="http://api.imgur.com/2/account/images"
+export ALBUMS_API_URL="http://api.imgur.com/2/account/albums"
 
 export ACCESS_TOKEN_FILE="valid_access_token.sh"
-export WEB_BROWSER_PATH=/usr/bin/google-chrome 
+
+export ALBUM_FILE_SUFFIX="_photo_to_album"
+export ALBUM_HASH_PREFIX="_album_hash"
+
